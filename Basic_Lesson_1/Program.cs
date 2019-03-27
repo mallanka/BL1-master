373ff94fb8c875a3a5e48818c0e92bb37eda11ae
mallanka <mallanka2@mail.ru>
2019-03-27 21:06:21 +0300

    Add PhotoCheckMethod

--- Basic_Lesson_1/Program.cs
+++ Basic_Lesson_1/Program.cs
@@ -8,10 +8,41 @@ namespace Basic_Lesson_1
 {
     class Program
     {
+        public static void PhotoCheckMethod()
+        {
+            Console.WriteLine("Now we need to check your photo for application form. Please, answer the questions only 'yes' or 'no'");
+            Console.WriteLine("Do you have a photo?");
+            if (Console.ReadLine().ToLower() == "yes")
+            {
+                Console.WriteLine("Is it less than three months?");
+                if (Console.ReadLine().ToLower() == "yes")
+                {
+                    Console.WriteLine("Is it in your passport photo?");
+                    if (Console.ReadLine().ToLower() == "no")
+                        Console.WriteLine("Great!");
+                    else
+                        Console.WriteLine("You need to make a new photo in photobooth");
+                }
+                else
+                    Console.WriteLine("You need to make a new photo in photobooth");
+            }
+            else
+                Console.WriteLine("You need to make a new photo in photobooth");
+
+            Console.WriteLine("Did you include your photo in form?");
+
+            while (Console.ReadLine().ToLower() != "yes")
+            {
+                Console.WriteLine("Please, include your photo in application form");
+                Console.WriteLine("Did you include your photo in form now?");
+            }
+
+            Console.WriteLine("Perfect! you can go to pay");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Здравсвуйте, меня зовут Василиса. Предоставьте список документов");
-
+            Program.PhotoCheckMethod();
             var documents = new  string [1];
             const int adultAge = 18;
 
